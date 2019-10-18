aefb7312bafca7517c1ec76d1a65c2463ae2f26a
velmurugan093 <rajmurugan093@gmail.com>
2019-10-18 19:12:20 +0530

    Scenario added with the tags

---
+++ src/test/java/com/demo/TestRunner/TestRunner1.java
@@ -0,0 +1,14 @@
+package com.demo.TestRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src\\test\\resources\\Demoguru.feature", glue="com.demo.stepdefinition",dryRun=false,monochrome = true,
+plugin= {"pretty","json:target/cucumber-reports/Cucumber.json"},tags= {"@SmokeTest,@RegressionTest"})
+
+public class TestRunner1 {
+
+}
